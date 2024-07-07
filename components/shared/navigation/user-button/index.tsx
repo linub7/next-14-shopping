8e009bbcdfda3b619c820d17351f34e0ddd05a4a
a1a2a3a4a5 <linub7@gmail.com>
2024-07-07 13:26:03 +0430

    add dark mode & light mode -> done

--- components/shared/navigation/user-button/index.tsx
+++ components/shared/navigation/user-button/index.tsx
@@ -1,21 +1,37 @@
 'use client';
 
+import { useState } from 'react';
 import { Session } from 'next-auth';
 import { signOut } from 'next-auth/react';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 import { LogOut, Moon, Settings, Sun, TruckIcon } from 'lucide-react';
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Switch } from '@/components/ui/switch';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 
 const NavUserButton = ({ user }: Session) => {
+  const [checked, setChecked] = useState(false);
+  const { setTheme, theme } = useTheme();
+
+  const handleModeState = () => {
+    switch (theme) {
+      case 'dark':
+        return setChecked(true);
+      case 'light':
+        return setChecked(false);
+      case 'system':
+        return setChecked(false);
+    }
+  };
+
   const handleSignout = () => signOut();
   if (!user) return null;
   return (
@@ -56,36 +72,59 @@ const NavUserButton = ({ user }: Session) => {
           </span>
         </div>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="group py-2 font-medium cursor-pointer transition-all duration-500">
+        <DropdownMenuItem className="group py-2 font-medium cursor-pointer transition-all duration-500 ease-in-out">
           <TruckIcon
             size={14}
             className="mr-3 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
           />{' '}
           My Orders
         </DropdownMenuItem>
-        <DropdownMenuItem className="group py-2 font-medium cursor-pointer transition-all duration-500">
+        <DropdownMenuItem className="group py-2 font-medium cursor-pointer transition-all duration-500 ease-in-out">
           <Settings
             size={14}
             className="mr-3 group-hover:rotate-180 transition-all duration-300 ease-in-out"
           />
           Settings
         </DropdownMenuItem>
-        <DropdownMenuItem className="py-2 font-medium cursor-pointer transition-all duration-500">
-          <div className="flex items-center gap-1">
-            <Sun size={14} />
-            <Moon size={14} />
-            <p>
-              Theme <span>theme</span>
-            </p>
-          </div>
-        </DropdownMenuItem>
+        {theme && (
+          <DropdownMenuItem className="py-2 font-medium cursor-pointer transition-all duration-500 ease-in-out">
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="flex items-center gap-1 group"
+            >
+              {theme === 'dark' ? (
+                <Moon
+                  size={14}
+                  className="group-hover:text-blue-400 group-hover:scale-125 transition-all duration-500 ease-in-out"
+                />
+              ) : (
+                <Sun
+                  size={14}
+                  className="group-hover:text-yellow-600 group-hover:rotate-180 transition-all duration-500 ease-in-out"
+                />
+              )}
+
+              <p className="dark:text-blue-400 text-secondary-foreground/75 ml-[10px] text-yellow-600">
+                {theme[0].toUpperCase() + theme.slice(1)} Mode
+              </p>
+              <Switch
+                className="scale-75 ml-4"
+                checked={checked}
+                onCheckedChange={(e) => {
+                  setChecked((prev) => !prev);
+                  e ? setTheme('dark') : setTheme('light');
+                }}
+              />
+            </div>
+          </DropdownMenuItem>
+        )}
         <DropdownMenuItem
           onClick={handleSignout}
-          className="group py-2 font-medium cursor-pointer transition-all duration-500 focus:bg-destructive/50"
+          className="group py-2 font-medium cursor-pointer transition-all duration-500 ease-in-out focus:bg-destructive/50"
         >
           <LogOut
             size={14}
-            className="mr-3 group-hover:scale-75 transition-all duration-300 ease-in-ou"
+            className="mr-3 group-hover:scale-75 transition-all duration-300 ease-in-out"
           />{' '}
           Sign out
         </DropdownMenuItem>
