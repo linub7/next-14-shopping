049a3b282b0a70dc0d9212611b6b46b0569fb5c0
a1a2a3a4a5 <linub7@gmail.com>
2024-07-13 14:30:30 +0430

    fix product variant forwardRef bug -> done

--- components/dashboard/products/product-variant/index.tsx
+++ components/dashboard/products/product-variant/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
+import { forwardRef, ReactNode, useState } from 'react';
 
 import {
   Dialog,
@@ -20,28 +20,33 @@ type Props = {
   variant?: VariantsWithImagesTags;
 };
 
-const ProductVariant = (props: Props) => {
-  const { editMode, children, productID, variant } = props;
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  return (
-    <Dialog modal={false} open={isModalOpen} onOpenChange={setIsModalOpen}>
-      <DialogTrigger>{children}</DialogTrigger>
-      <DialogContent className="lg:max-w-screen-lg overflow-y-scroll max-h-[640px] rounded-md">
-        <DialogHeader>
-          <DialogTitle>{editMode ? 'Edit' : 'Create'} your variant</DialogTitle>
-          <DialogDescription>
-            Manage your product variants. You can add tags, images and more.
-          </DialogDescription>
-        </DialogHeader>
-        <AddVariantForm
-          editMode={editMode}
-          productID={productID}
-          variant={variant}
-          setIsModalOpen={setIsModalOpen}
-        />
-      </DialogContent>
-    </Dialog>
-  );
-};
+const ProductVariant = forwardRef<HTMLInputElement, Props>(
+  ({ editMode, children, productID, variant }, ref) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    return (
+      <Dialog modal={false} open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogTrigger>{children}</DialogTrigger>
+        <DialogContent className="lg:max-w-screen-lg overflow-y-scroll max-h-[640px] rounded-md">
+          <DialogHeader>
+            <DialogTitle>
+              {editMode ? 'Edit' : 'Create'} your variant
+            </DialogTitle>
+            <DialogDescription>
+              Manage your product variants. You can add tags, images and more.
+            </DialogDescription>
+          </DialogHeader>
+          <AddVariantForm
+            editMode={editMode}
+            productID={productID}
+            variant={variant}
+            setIsModalOpen={setIsModalOpen}
+          />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+);
+
+ProductVariant.displayName = 'ProductVariant';
 
 export default ProductVariant;
